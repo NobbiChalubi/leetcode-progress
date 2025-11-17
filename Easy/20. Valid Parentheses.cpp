1d4ff2723767a6cd299020ded455adf120c29a1d
NoobisChalubis <krispygabe9@gmail.com>
2025-11-17 17:52:28 -0500

    Add Folders

---
+++ Easy/20. Valid Parentheses.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+#include <stack>
+
+
+
+bool isValid(std::string& s) {
+	if (s.size() % 2 != 0) { return false; }
+	std::stack<char> stack;
+	static std::unordered_map<char, char> key{
+		{')', '('},
+		{'}','{'},
+		{']','['},
+	};
+
+	for (char c : s) {
+		if (c == '(' or c == '[' or c == '{') {
+			stack.push(c);
+		}
+		else {
+			if (stack.empty()) { return false; }
+			else if (stack.top() != key[c]) { return false; }
+			else if (stack.top() == key[c]) { stack.pop(); continue; }
+		}
+	}
+	if (stack.empty()) { return true; }
+	else { return false; }
+
+}
+
+int main() {
+	std::string input;
+	std::cin >> input;
+	std::cout << isValid(input);
+	return 0;
+}
